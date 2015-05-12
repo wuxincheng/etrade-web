4cf902dcf1f61f4465d951dcf364ff6f39ad2a20
wuxincheng <wuxinchenghappy@gmail.com>
2015-05-12 15:01:43 +0800

    修改包的架构, 为适应更多的项目

---
+++ src/main/java/com/trade/web/controller/AccountController.java
@@ -0,0 +1,30 @@
+package com.trade.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户账户中心
+ * 
+ * @author wuxincheng
+ *
+ */
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+	private static Logger logger = LoggerFactory.getLogger(AccountController.class);
+	
+	@RequestMapping(value = "/main")
+	public String main(Model model) {
+		logger.info("个人账户主界面");
+		
+		model.addAttribute("flag", "account");
+		
+		return "account/main";
+	}
+	
+}
---
+++ src/main/java/com/trade/web/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.trade.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+	private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+	@RequestMapping(value = "/")
+	public String index(Model model) {
+		logger.info("前端首页");
+		
+		model.addAttribute("flag", "index");
+		
+		return "index";
+	}
+	
+}
